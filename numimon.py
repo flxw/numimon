d654421405bfd25e5f918599df2bc437945de35c
Felix Wolff <felix.wolff2@student.hpi.de>
2017-07-17 15:20:45 +0200

    Initial commit

---
+++ numimon.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+                           node0           node1           node2           node3
+numa_hit               207950520       180112803       230784380       171132101
+numa_miss                      0               0               0               0
+numa_foreign                   0               0               0               0
+interleave_hit             20155           20494           20640           20495
+local_node             203187161       143462558       218058367       149127528
+other_node               4763359        36650245        12726013        22004573
